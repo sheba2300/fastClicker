ba65b27ab856c3fac2f1a9def8d9112dc44daf69
sheba sow <ssow8171@bths.edu>
2019-01-06 23:17:28 -0500

    done

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,16 +1,64 @@
 package sample;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.animation.AnimationTimer;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Controller {
     int counter = 0;
     @FXML
-    private Button clicker;
-    public void pressButton()
-    {
-      counter++;
+    private ImageView clicker;
+    @FXML
+    private Button start;
+    @FXML
+    private Label countDisplay;
+    boolean canPlay = false;
+    int highScore = 0;
+
+
+
+        public void startGame()
+        {
+            //checks to see if change is in session
+            //once button is clicked timer starts and each click counted using the counter varible
+            //once Timer is down the count is displayed and compared with best score
+            if(!canPlay) {
+                counter = 0;
+                canPlay = true;
+                start.setText(" Goo!");
+                Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(10), ev -> {
+                    canPlay = false;
+                    countDisplay.setText("Your final score is " + counter);
+                    if (highScore < counter) {
+                        highScore = counter;
+                    }
+                    start.setText("Start");
+                }
+                ));
+                timeline.play();
+            }
+        }
+        public void pressButton()
+    {//increments the counter and updates the display
+       if(canPlay) {
+           counter++;
+           countDisplay.setText("SCORE: " + counter + " BEST: " + highScore);
+       }
     }
 
 }
